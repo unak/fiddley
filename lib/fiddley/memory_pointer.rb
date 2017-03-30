b851430da50dc74a7370b8b6e3db9a54f03afa7d
U.Nakamura <usa@garbagecollect.jp>
2017-03-31 05:51:57 +0900

    Implement methods of MemoryPointer

--- lib/fiddley/memory_pointer.rb
+++ lib/fiddley/memory_pointer.rb
@@ -6,86 +6,134 @@ module Fiddley
     if defined?(Fiddley::RefineStringUnpack1)
       using Fiddley::RefineStringUnpack1
     end
+
     include Fiddley::Utils
 
-    def initialize(type, num = 1)
-      size = type2size(type) * num
-      @ptr = Fiddle::Pointer.malloc(size)
+    def initialize(type, num = 1, clear = true)
+      @size = type2size(type) * num
+      @ptr = Fiddle::Pointer.malloc(@size)
     end
 
-    def write_int16(val)
-      @ptr[0, 2] = [val].pack('s')
-    end
-
-    def write_int32(val)
-      @ptr[0, 4] = [val].pack('l')
-    end
-
-    def write_array_of_uint32(ary)
-      write_string(ary.pack('L*'))
-    end
-
-    def write_string(str)
-      @ptr[0, str.bytesize] = str
-    end
-
-    def read_bytes(size)
-      @ptr[0, size]
-    end
-
-    def read_int8
-      get_int8(0)
-    end
-
-    def read_uint8
-      get_uint8(0)
-    end
-
-    def read_int16
-      get_int16(0)
-    end
-
-    def read_uint16
-      get_uint16(0)
-    end
-
-    def read_int32
-      get_int32(0)
-    end
-
-    def read_uint32
-      get_uint32(0)
-    end
-
-    def get_int8(offset)
-      @ptr[offset, 1].unpack1('c')
-    end
-
-    def get_uint8(offset)
-      @ptr[offset, 1].unpack1('C')
-    end
-
-    def get_int16(offset)
-      @ptr[offset, 2].unpack1('s')
-    end
-
-    def get_uint16(offset)
-      @ptr[offset, 2].unpack1('S')
-    end
-
-    def get_int32(offset)
-      @ptr[offset, 4].unpack1('l')
-    end
-
-    def get_uint32(offset)
-      @ptr[offset, 4].unpack1('L')
-    end
-
-    alias read_int read_int32
-    alias get_int get_int32
+    attr_reader :size
 
     def to_ptr
       @ptr
     end
+
+    {8 => 'c', 16 => 's', 32 => 'l', 64 => 'q'}.each_pair do |bits, form|
+      bytes = bits/8
+
+      define_method("put_int#{bits}") do |offset, val|
+        @ptr[offset, bytes] = [val].pack(form)
+      end
+
+      define_method("write_int#{bits}") do |val|
+        __send__("put_int#{bits}", 0, val)
+      end
+
+      define_method("put_array_of_int#{bits}") do |offset, ary|
+        put_bytes(offset, ary.pack(form + '*'))
+      end
+
+      define_method("write_array_of_int#{bits}") do |ary|
+        __send__("put_array_of_int#{bits}", 0, ary)
+      end
+
+      define_method("get_int#{bits}") do |offset|
+        @ptr[offset, bytes].unpack1(form)
+      end
+
+      define_method("read_int#{bits}") do
+        __send__("get_int#{bits}", 0)
+      end
+
+      define_method("get_array_of_int#{bits}") do |offset, num|
+        @ptr[offset, bytes*num].unpack1(form + '*')
+      end
+
+      define_method("read_array_of_int#{bits}") do |num|
+        __send__("get_array_of_int#{bits}", 0, num)
+      end
+
+      form2 = form.upcase
+
+      define_method("put_uint#{bits}") do |offset, val|
+        @ptr[offset, bytes] = [val].pack(form2)
+      end
+
+      define_method("write_uint#{bits}") do |val|
+        __send__("put_uint#{bits}", 0, val)
+      end
+
+      define_method("put_array_of_uint#{bits}") do |offset, ary|
+        put_bytes(offset, ary.pack(form2 + '*'))
+      end
+
+      define_method("write_array_of_uint#{bits}") do |ary|
+        __send__("put_array_of_uint#{bits}", 0, ary)
+      end
+
+      define_method("get_uint#{bits}") do |offset|
+        @ptr[offset, bytes].unpack1(form2)
+      end
+
+      define_method("read_uint#{bits}") do
+        __send__("get_uint#{bits}", 0)
+      end
+
+      define_method("get_array_of_uint#{bits}") do |offset, num|
+        @ptr[offset, bytes*num].unpack1(form2 + '*')
+      end
+
+      define_method("read_array_of_uint#{bits}") do |num|
+        __send__("get_array_of_uint#{bits}", 0, num)
+      end
+    end
+
+    def put_bytes(offset, str, len = nil)
+      @ptr[offset, len ? len : str.bytesize] = len ? str[0, len] : str
+    end
+
+    def write_bytes(str, len = nil)
+      put_bytes(0, str, len)
+    end
+
+    def get_bytes(offset, len)
+      @ptr[offset, len]
+    end
+
+    def read_bytes(len)
+      get_bytes(0, len)
+    end
+
+    def put_string(offset, str)
+      @ptr[offset, str.bytesize] = str
+    end
+
+    def write_string(str, len = nil)
+      put_string(0, len ? str[0, len] : str)
+    end
+
+    def write_string_length(str, len)
+      put_string(0, str[0, len])
+    end
+
+    def get_string(offset, len = nil)
+      @ptr[offset, len ? len : @size - offset]
+    end
+
+    def read_string(len = nil)
+      get_string(0, len)
+    end
+
+    alias put_int put_int32
+    alias write_int write_int32
+    alias get_int get_int32
+    alias read_int read_int32
+
+    alias put_uint put_uint32
+    alias write_uint write_uint32
+    alias get_uint get_uint32
+    alias read_uint read_uint32
   end
 end

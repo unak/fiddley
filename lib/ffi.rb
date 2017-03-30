7ac2cc545bb836d61e7b8ddf254cc3e963b290f5
U.Nakamura <usa@garbagecollect.jp>
2017-03-30 20:24:30 +0900

    Define FFI compatible class only when required 'ffi'

--- lib/ffi.rb
+++ lib/ffi.rb
@@ -1,1 +1,3 @@
 require "fiddley"
+
+FFI = Fiddley
--- lib/fiddley.rb
+++ lib/fiddley.rb
@@ -2,8 +2,6 @@ module Fiddley
   VERSION = "0.0.1"
 end
 
-FFI = Fiddley
-
 require "fiddley/function"
 require "fiddley/memory_pointer"
 require "fiddley/library"
